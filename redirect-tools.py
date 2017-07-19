1712cea5e0042ada2ae3d2bf69352725da9ace0a
Michael Peto <michael.peto10@gmail.com>
2017-07-19 10:51:31 -0400

    first commit

---
+++ redirect-tools.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+from html.parser import HTMLParser #https://pypi.python.org/pypi/HTMLParser/0.0.2
+import pandas #https://pypi.python.org/pypi/pandas/0.20.1
+import requests #https://pypi.python.org/pypi/requests/2.14.2
+import configparser #https://pypi.python.org/pypi/configparser
+from xlutils.copy import copy   #https://pypi.python.org/pypi/xlutils/2.0.0
+from xlrd import open_workbook #https://pypi.python.org/pypi/xlrd
+from xlwt import easyxf #https://pypi.python.org/pypi/xlwt/1.2.0
+
+# read from config file
+config= configparser.ConfigParser()
+config.read('settings.cfg')
+
+input_file, URL_column, redirect_column, keep_original_comments, save_every_row, check_for_sharepoint_404, verify_SSL, new_root_domain, complex_regex, old_root_domains = str(config['EXCEL FILE']['input_file']), str(config['EXCEL FILE']['URL_column']), str(config['EXCEL FILE']['redirect_column']), (config.getboolean('EXCEL FILE','keep_original_comments')), (config.getboolean('EXCEL FILE','save_every_row')), (config.getboolean('TESTING','check_for_sharepoint_404')), (config.getboolean('TESTING','verify_SSL')), str(config['RULE CREATION']['new_root_domain']), (config.getboolean('RULE CREATION','complex_regex')), dict(config.items('OLD ROOT DOMAINS'))
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def __sanitize_URLs__(rule, isRegex):
+    current_redirect = strip_tags(str(rule[0]).lower().strip(" ><"))
+    future_redirect = strip_tags(str(rule[1]).lower().strip(" ><"))
+    domain_list = old_root_domains.values()
+    for domain in domain_list:
+        current_redirect = current_redirect.replace(domain, "")
+    if(isRegex):
+        current_redirect = current_redirect.replace(".", "\.").lstrip('\/').rstrip('\/').replace("/", "\/")
+    return current_redirect, future_redirect
+
+def __write_rules_to_file__(isRegexRule, output_file, format_syntax):
+    cols_to_parse=str(URL_column + ", " + redirect_column)
+    rf = pandas.read_excel(input_file, index_col=None, parse_cols=cols_to_parse)
+    list_of_redirects = list(rf.values)
+    previous_URLS = []
+    file_to_write = open(output_file, "w+")
+    to_prepend = format_syntax[0]
+    to_insert = format_syntax[1]
+    to_append = format_syntax [2]
+
+    for rule in list_of_redirects:
+        current_redirect, future_redirect = __sanitize_URLs__(rule, isRegexRule)
+        if(isRegexRule):
+            to_insert = str('" stopProcessing="true">\r\n\t<match url="(' + current_redirect + ')(.*)" ignoreCase="true"/>\r\n\t<action type ="Redirect" url="' + new_root_domain)
+            if(complex_regex):
+                to_append = str('{R:2}" redirectType = "Permanent" />\r\n</rule>')
+            else:
+                to_append = str( '" redirectType = "Permanent" />\r\n</rule>')
+        print (current_redirect + " = " + future_redirect)
+        if(current_redirect not in previous_URLS):
+            file_to_write.write(str(to_prepend + current_redirect + to_insert + future_redirect + to_append + "\r\n"))
+        else:
+            print("DUPLICATE ENTRY")
+        previous_URLS.append(current_redirect)
+    file_to_write.close()
+
+def __test_redirects__():
+    df = pandas.read_excel(input_file, index_col=None, parse_cols=URL_column)
+    print(df) #prints summary of column with URLs to test
+    list_of_links = list(df.values.flatten())
+    requests.packages.urllib3.disable_warnings() #get rid of "SSL cert check disabled" warnings
+    workbook = open_workbook(input_file, formatting_info=True) #only works with .xls documents currently, as per xlrd documentation
+    write_copy = copy(workbook)
+    sheet = write_copy.get_sheet(0)
+    sheet.cell_overwrite_ok = True
+    row_number = 1
+
+    for link in list_of_links:
+        current_link = str(link).strip() #update link from spreadsheet
+        try:
+            print("Requested URL: " + current_link)
+            r = requests.get(current_link, verify=verify_SSL) #attempt to get URL + ignore testing SSL cretificate
+            print("Redirect URL: " + str(r.url))
+            status_code = str(r.status_code)
+            if(check_for_sharepoint_404): #if enabled, check for standard sharepoint 404 page
+                if(status_code == "200"):
+                    try:
+                        title = (str(r.text)).split('<meta name="description" content="')[1].split('" />')[0] #parse html
+                        page_title = (title[3:-3])
+                        if(page_title=="404 Error"):
+                            print("URL is generic Sitepoint 404 page!")
+                            status_code = "404"
+                            if(not keep_original_comments):
+                                sheet.write(row_number, 3, "Sharepoint 404, page does not exist.")
+                    except:
+                        pass
+            print("Status code: " + status_code)
+            sheet.write(row_number, 1, r.url) #write URL redirect
+            sheet.write(row_number, 2, status_code) #write status code
+        except requests.exceptions.RequestException as e: #catch failed HTTP request
+            status_code = "404" #return a 404; this should be the only error triggering an exception
+            print("ERROR: " + str(e))
+            print("Redirect URL: N/A")
+            print("Status code: " + status_code)
+            sheet.write(row_number, 3, str(e)) #write error to comments
+            sheet.write(row_number, 2, status_code) #write response code
+            sheet.write(row_number, 1, "") #write blank redirect url
+        if(save_every_row):
+            write_copy.save(input_file + '.out' + os.path.splitext(input_file)[-1]) #saves excel doc after every row if enabled
+        row_number+=1 #increment row number so next loop cycle writes to next row in excel sheet
+        print("===========")
+    write_copy.save(input_file + '.out' + os.path.splitext(input_file)[-1]) #save file after all changes are made
+    print("===================LIST COMPLETE=========================")
+
+def __create_redirect_map__():
+    print("=====REDIRECT MAP======")
+    isRegexRule = False
+    output_file = "rewritemaps.config.txt"
+    format_syntax = ['<rewriteMaps>\r\n\t<rewriteMap name="Redirects">\r\n\t\t<add key="', '" value="', '" />\r\n\t</rewriteMap>\r\n</rewriteMaps>']
+    __write_rules_to_file__(isRegexRule, output_file, format_syntax)
+    print("===================REDIRECT MAP CREATED======================")
+
+def __create_htaccess__():
+    print("=====HTACCESS CREATOR======")
+    isRegexRule = False
+    output_file = ".htaccess"
+    format_syntax = ['Redirect 301 ', " ", ""]
+    __write_rules_to_file__(isRegexRule, output_file, format_syntax)
+    print("===================HTACCESS CREATED======================")
+
+def __create_redirect_rules__():
+    print("=====REDIRECT RULES======")
+    isRegexRule = True
+    output_file = "rewriterules.txt"
+    format_syntax = [ '<rule name="', '', '']
+    __write_rules_to_file__(isRegexRule, output_file, format_syntax)
+    print("===================REDIRECT RULES CREATED======================")
+
+def __help__():
+    print("This script takes at least one parameter, depending on the desired function.")
+    print("Available functions:")
+    print("test                     -           tests redirects and logs results to new file, according to options in settings.cfg")
+    print("create_map               -           creates IIS redirect rule map according to links in spreadsheet and options in settings.cfg")
+    print("create_htaccess          -           creates htaccess redirect rules according to spreadsheet")
+    print("create_rules             -           creates regex IIS rewrite rules from spreadsheet")
+
+# CALL HANDLING #
+if (len(sys.argv) < 2):
+    __help__()
+elif((sys.argv[1]).lower() == "test"):
+    __test_redirects__()
+elif((sys.argv[1]).lower() == "create_map"):
+    __create_redirect_map__()
+elif((sys.argv[1]).lower() == "create_htaccess"):
+    __create_htaccess__()
+elif((sys.argv[1]).lower() == "create_rules"):
+    __create_redirect_rules__()
+else:
+    __help__()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='redirect-tools',
+    version='1.0',
+    packages=['redirect-tools'],
+    scripts=['redirect-tools.py'],
+    license='LICENSE.txt',
+    description='Tools for dealing with URL redirects.'
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'pandas>=0.20.1'
+        'requests>=2.14.2'
+        'configparser>=3.5.0'
+        'xlutils>=2.0.0'
+        'xlrd>=1.0.0'
+        'xlwt>=1.2.0'
+    ],
+)
